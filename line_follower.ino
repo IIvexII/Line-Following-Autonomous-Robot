5d6f1dd989ecc5100e6543f97e1b4db8eab21f09
Zafeer Hafeez <mrh4ck3r1738@gmail.com>
2022-04-06 06:01:09 +0500

    Update line_follower.ino

--- line_follower.ino
+++ line_follower.ino
@@ -11,68 +11,64 @@ void setup()
 }
 void loop()
 {
-  leftMotor.setSpeed(150);
-  leftMotor.farward();
-  rightMotor.setSpeed(150);
-  rightMotor.farward();
-//  int sp = 150;
-//  int leftSpeed = 150;
-//  int rightSpeed = 150;
-//  leftMotor.farward();
-//  rightMotor.farward();
-//  bool leftV = leftSensor.getValue();
-//  bool centerV = middleSensor.getValue();
-//  bool rightV = rightSensor.getValue();
-//
-//  if (leftV == 1 && centerV == 0 && rightV == 1)
-//  {
-//    Serial.println("farward");
-//    leftMotor.setSpeed(leftSpeed);
-//    rightMotor.setSpeed(rightSpeed);
-//    // Move farward
-//    rightMotor.farward();
-//    leftMotor.farward();
-//  }
-//  else if (leftV == 0 && centerV == 0 && rightV == 0)
-//  {
-//    Serial.println("Stop all white");
-//    // Stop the car
-//    leftMotor.setSpeed(0);
-//    rightMotor.setSpeed(0);
-//  }
-//  else if (leftV == 1 && centerV == 1 && rightV == 1)
-//  {
-//    Serial.println("Stop all black");
-//    // Stop the car
-//    leftMotor.setSpeed(0);
-//    rightMotor.setSpeed(0);
-//  }
-//  else if (leftV == 0 && centerV == 0 && rightV == 1)
-//  {
-//    Serial.println("Left1");
-//    // Rotate left
-//    leftMotor.setSpeed(0);
-//    rightMotor.setSpeed(rightSpeed);
-//  }
-//  else if (leftV == 1 && centerV == 0 && rightV == 0)
-//  {
-//    Serial.println("right");
-//    // Rotate right
-//    leftMotor.setSpeed(leftSpeed);
-//    rightMotor.setSpeed(0);
-//  }
-//  else if (leftV == 0 && centerV == 1 && rightV == 1)
-//  {
-//    Serial.println("Left2");
-//    // Rotate left
-//    leftMotor.setSpeed(0);
-//    rightMotor.setSpeed(rightSpeed);
-//  }
-//  else if (leftV == 1 && centerV == 1 && rightV == 0)
-//  {
-//    Serial.println("Right2");
-//    // Rotate right
-//    leftMotor.setSpeed(leftSpeed);
-//    rightMotor.setSpeed(0);
-//  }
+ int sp = 150;
+ int leftSpeed = 150;
+ int rightSpeed = 150;
+ leftMotor.farward();
+ rightMotor.farward();
+ bool leftV = leftSensor.getValue();
+ bool centerV = middleSensor.getValue();
+ bool rightV = rightSensor.getValue();
+
+ if (leftV == 1 && centerV == 0 && rightV == 1)
+ {
+   Serial.println("farward");
+   leftMotor.setSpeed(leftSpeed);
+   rightMotor.setSpeed(rightSpeed);
+   // Move farward
+   rightMotor.farward();
+   leftMotor.farward();
+ }
+ else if (leftV == 0 && centerV == 0 && rightV == 0)
+ {
+   Serial.println("Stop all white");
+   // Stop the car
+   leftMotor.setSpeed(0);
+   rightMotor.setSpeed(0);
+ }
+ else if (leftV == 1 && centerV == 1 && rightV == 1)
+ {
+   Serial.println("Stop all black");
+   // Stop the car
+   leftMotor.setSpeed(0);
+   rightMotor.setSpeed(0);
+ }
+ else if (leftV == 0 && centerV == 0 && rightV == 1)
+ {
+   Serial.println("Left1");
+   // Rotate left
+   leftMotor.setSpeed(0);
+   rightMotor.setSpeed(rightSpeed);
+ }
+ else if (leftV == 1 && centerV == 0 && rightV == 0)
+ {
+   Serial.println("right");
+   // Rotate right
+   leftMotor.setSpeed(leftSpeed);
+   rightMotor.setSpeed(0);
+ }
+ else if (leftV == 0 && centerV == 1 && rightV == 1)
+ {
+   Serial.println("Left2");
+   // Rotate left
+   leftMotor.setSpeed(0);
+   rightMotor.setSpeed(rightSpeed);
+ }
+ else if (leftV == 1 && centerV == 1 && rightV == 0)
+ {
+   Serial.println("Right2");
+   // Rotate right
+   leftMotor.setSpeed(leftSpeed);
+   rightMotor.setSpeed(0);
+ }
 }
